9f19f7f863561b0f3bf17aaa997f72a464804d29
Dean Vo <deaanvo@gmail.com>
2020-10-13 15:26:25 -0700

    joined late. will continue later.

---
+++ javascript-data-model/main.js
@@ -0,0 +1,40 @@
+/* exported orderHistory */
+
+var orderHistory = [
+  {
+    orderPlaced: 'August 4, 2020',
+    price: [31.55],
+    deliveredBy: 'August 8, 2020',
+    product: ['JavaScript for impatient programmers'],
+    orderNumber: '114-3941689-8772232',
+    shipTo: 'JS Masher',
+    returnBy: 'September 7, 2020'
+  },
+  {
+    orderPlaced: 'July 19, 2020',
+    price: [41.33],
+    deliveredBy: 'July 20, 2020',
+    product: ['The Timeless Way of Building'],
+    orderNumber: '113-9984268-1280257',
+    shipTo: 'JS Masher',
+    returnBy: 'August 19, 2020'
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    price: [15.98],
+    deliveredBy: 'July 7, 2020',
+    product: ['Gamecube Controller Adapter'],
+    orderNumber: '114-2875557-9059409',
+    shipTo: 'JS Masher',
+    returnBy: 'August 5, 2020'
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    price: [94.95, 33.99],
+    deliveredBy: 'July 5, 2020',
+    product: ['GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sql'],
+    orderNumber: '113-2883177-2648248',
+    shipTo: 'JS Masher',
+    returnBy: ['August 4, 2020', 'August 4, 2020']
+  }
+];
