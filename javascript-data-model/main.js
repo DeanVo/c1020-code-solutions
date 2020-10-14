23316358d5dcc1833c7dac3e45fa48627a2109aa
Dean Vo <deaanvo@gmail.com>
2020-10-13 21:08:59 -0700

    completed javascript-data-model

--- javascript-data-model/main.js
+++ javascript-data-model/main.js
@@ -3,38 +3,75 @@
 var orderHistory = [
   {
     orderPlaced: 'August 4, 2020',
-    price: [31.55],
-    deliveredBy: 'August 8, 2020',
-    product: ['JavaScript for impatient programmers'],
+    orderTotal: 34,
     orderNumber: '114-3941689-8772232',
+    deliveredBy: 'August 8, 2020',
     shipTo: 'JS Masher',
-    returnBy: 'September 7, 2020'
+    product: [
+      {
+        type: 'book',
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnBy: 'September 7, 2020',
+        price: 31.55
+      }
+    ]
   },
+
   {
     orderPlaced: 'July 19, 2020',
-    price: [41.33],
-    deliveredBy: 'July 20, 2020',
-    product: ['The Timeless Way of Building'],
+    orderTotal: 44.53,
     orderNumber: '113-9984268-1280257',
+    deliveredBy: 'July 20, 2020',
     shipTo: 'JS Masher',
-    returnBy: 'August 19, 2020'
+    product: [
+      {
+        type: 'book',
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnBy: 'August 19, 2020',
+        price: 41.33
+      }
+    ]
   },
+
   {
+
     orderPlaced: 'July 4, 2020',
-    price: [15.98],
-    deliveredBy: 'July 7, 2020',
-    product: ['Gamecube Controller Adapter'],
+    orderTotal: 17.22,
     orderNumber: '114-2875557-9059409',
+    deliveredBy: 'July 7, 2020',
     shipTo: 'JS Masher',
-    returnBy: 'August 5, 2020'
+    product: [
+      {
+        type: 'hardware',
+        title: 'GameCube Controller Adapater. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        returnBy: 'August 5, 2020',
+        price: 15.98
+      }
+    ]
   },
+
   {
+
     orderPlaced: 'July 3, 2020',
-    price: [94.95, 33.99],
-    deliveredBy: 'July 5, 2020',
-    product: ['GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)', 'The Art of Sql'],
+    orderTotal: 138.93,
     orderNumber: '113-2883177-2648248',
+    deliveredBy: 'July 5, 2020',
     shipTo: 'JS Masher',
-    returnBy: ['August 4, 2020', 'August 4, 2020']
+    product: [
+      {
+        type: 'hardware',
+        title: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        returnBy: 'August 4, 2020',
+        price: 94.95
+      },
+      {
+        type: 'hardware',
+        title: 'The Art of Sql',
+        returnBy: 'August 4, 2020',
+        price: 33.99
+      }
+    ]
   }
 ];
