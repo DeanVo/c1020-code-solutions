db55a05af988edc44fe5c0d5115f09e8640ee6b5
Dean Vo <deaanvo@gmail.com>
2020-10-16 12:41:22 -0700

    adjusted code block of for loop - removed for/in loop

--- javascript-loops-practice/get-student-names.js
+++ javascript-loops-practice/get-student-names.js
@@ -2,11 +2,8 @@
 
 function getStudentNames(students) {
   var values = [];
-  var x;
   for (var i = 0; i < students.length; i++) {
-    for (x in students[i]) {
-      values.push(students[i][x]);
-    }
+    values.push(students[i].name);
   }
   return values;
 }
