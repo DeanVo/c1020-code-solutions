6e5512fd2e9baca10dd1e4e8dd6b1c81be5b9be4
Dean Vo <deaanvo@gmail.com>
2020-10-19 10:31:38 -0700

    completed dom-querying exercise

---
+++ dom-querying/main.js
@@ -0,0 +1,12 @@
+console.log('queryS DOM h1: ', document.querySelector('h1'));
+
+console.dir(document.querySelector('h1'));
+
+console.log('queryS DOM id - explanation: ', document.querySelector('#explanation'));
+console.dir(document.querySelector('#explanation'));
+
+console.log('queryS DOM class - hint: ', document.querySelector('.hint'));
+console.dir(document.querySelector('.hint'));
+
+console.log('querySA DOM type - p: ', document.querySelectorAll('p'));
+console.log('querySA DOM class - example-link: ', document.querySelectorAll('.example-link'));
