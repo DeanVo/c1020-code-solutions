c02548bba7f2289d1f2857a73faf01f024b1c429
Dean Vo <deaanvo@gmail.com>
2020-10-23 10:43:03 -0700

    had an ephiphany on how to do get-words.js

--- string-manipulation-1/src/get-words.js
+++ string-manipulation-1/src/get-words.js
@@ -1,9 +1,17 @@
 /* eslint-disable no-unused-vars */
 
+// function getWords(string) {
+//   var words = string.split(' ');
+//   if (string === '') {
+//     words = string.split('');
+//   }
+//   return words;
+// }
+
 function getWords(string) {
-  var words = string.split(' ');
-  if (string === '') {
-    words = string.split('');
+  var newArray = [];
+  if (string.length > 0) {
+    newArray = string.split(' ');
   }
-  return words;
+  return newArray;
 }
