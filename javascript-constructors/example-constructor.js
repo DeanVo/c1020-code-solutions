7d2cc2aee9d1f2dfc5924fa427ac1c7f9cdb71da
Dean Vo <deaanvo@gmail.com>
2020-11-03 11:02:23 -0800

    updated exampleconstructor

--- javascript-constructors/example-constructor.js
+++ javascript-constructors/example-constructor.js
@@ -1,7 +1,8 @@
 function ExampleConstructor() {
-  console.log('value of ExampleConstructor prototype: ', ExampleConstructor.prototype);
-  console.log('typeof ExampleConstructor prototype: ', typeof ExampleConstructor.prototype);
 }
+
+console.log('value of ExampleConstructor prototype: ', ExampleConstructor.prototype);
+console.log('typeof ExampleConstructor prototype: ', typeof ExampleConstructor.prototype);
 
 var constructor = new ExampleConstructor();
 console.log('value of constructor: ', constructor);
