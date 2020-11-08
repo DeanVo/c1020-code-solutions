b03f41dccf743f5590319c23dfab2f65b1b10a5c
Dean Vo <deaanvo@gmail.com>
2020-11-07 16:56:37 -0800

    working on titleCase

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,11 +1,73 @@
 /* eslint-disable no-unused-vars */
 
+// function titleCase(title) {
+//   var stringArray = title.split(' ');
+//   var lastIndex = stringArray.length - 1;
+//   var newArray = [];
+
+//   for (var i = 0; i < stringArray.length; i++) {
+//     if (stringArray[i].length <= 3 && stringArray[i] !== stringArray[0] && stringArray[i] !== stringArray[lastIndex]) {
+//       newArray.push(stringArray[i].toLowerCase());
+//     } else {
+//       newArray.push(stringArray[i].charAt(0).toUpperCase() + stringArray[i].substring(1));
+//     }
+//   }
+//   return newArray.join(' ');
+// }
+
+// capitalize the first word if the title and of any subtitle
+// capitalize all "major" words in the title, including the second part of hyphenated major words
+// capitalize all words of four letters or more
+
+// function titleCase(title) {
+//   var stringArray = title.split(' ');
+//   var newArray = [];
+
+//   for (var i = 0; i < stringArray.length; i++) {
+//     if (stringArray[i].length >= 4 && stringArray[i] !== stringArray[i].includes('-') && stringArray[i].toLowerCase() !== 'javascript') {
+//       newArray.push(stringArray[i].charAt(0).toUpperCase() + stringArray[i].substring(1));
+//     } else if (stringArray[i].toLowerCase().includes('javascript')) {
+//       newArray.push('JavaScript');
+//     } else if (stringArray[i].toLowerCase() === 'api') {
+//       newArray.push('API');
+//     } else if (stringArray[i].toLowerCase() === 'web') {
+//       newArray.push('Web');
+//     } else if (stringArray[i].includes('-')) {
+//       var hyphenArray = stringArray[i].split('-');
+//       newArray.push(hyphenArray[0].charAt(0).toUpperCase() + hyphenArray[0].substring(1) + '-' + hyphenArray[1].charAt(0).toUpperCase() + hyphenArray[1].substring(1));
+//     } else {
+//       newArray.push(stringArray[i].toLowerCase());
+//     }
+//   }
+//   newArray = newArray.join(' ');
+
+//   return newArray;
+// }
+
 function titleCase(title) {
   var stringArray = title.split(' ');
   var newArray = [];
+  var lastIndex = stringArray.length - 1;
 
   for (var i = 0; i < stringArray.length; i++) {
-    newArray.push(stringArray[i].charAt(0).toUpperCase() + stringArray[i].substring(1));
+    if (stringArray[i].includes('-')) {
+      var hyphenArray = stringArray[i].split('-');
+      newArray.push(hyphenArray[0].charAt(0).toUpperCase() + hyphenArray[0].substring(1) + '-' + hyphenArray[1].charAt(0).toUpperCase() + hyphenArray[1].substring(1));
+    } else if (stringArray[i].includes(':')) {
+      newArray.push(stringArray[i] + ' ' + stringArray[i + 1].charAt(0).toUpperCase() + stringArray[i + 1].substring(1));
+    } else if (stringArray[i].length >= 4 && stringArray[i] && stringArray[i].toLowerCase() !== 'javascript') {
+      newArray.push(stringArray[i].charAt(0).toUpperCase() + stringArray[i].substring(1));
+    } else if (stringArray[i].toLowerCase().includes('javascript')) {
+      newArray.push('JavaScript');
+    } else if (stringArray[i].toLowerCase() === 'api') {
+      newArray.push('API');
+    } else if (stringArray[i].toLowerCase() === 'web') {
+      newArray.push('Web');
+    } else if (stringArray[i] !== stringArray[0] && stringArray[i] !== stringArray[lastIndex]) {
+      newArray.push(stringArray[i].toLowerCase());
+    }
   }
-  return newArray.join(' ');
+  newArray = newArray.join(' ');
+
+  return newArray;
 }
