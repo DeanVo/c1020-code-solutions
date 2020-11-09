5c002cd459a0f6053ebb83b603349d8284d74838
Dean Vo <deaanvo@gmail.com>
2020-11-08 20:14:52 -0800

    finished basic functionality

---
+++ javascript-typing-tutor/main.js
@@ -0,0 +1,14 @@
+var $typingSpan = document.querySelectorAll('span');
+var index = 0;
+
+function typingInstructor(e) {
+  if (event.key === $typingSpan[index].textContent) {
+    $typingSpan[index].className = 'correct';
+    $typingSpan[index + 1].className = 'underline';
+    index++;
+  } else {
+    $typingSpan[index].className = 'incorrect underline-red';
+  }
+}
+
+document.addEventListener('keydown', typingInstructor);
