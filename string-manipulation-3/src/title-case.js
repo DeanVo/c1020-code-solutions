e7c82e33bc25a07db46f18602fa42f21a778f7ae
Dean Vo <deaanvo@gmail.com>
2020-11-22 17:22:36 -0800

    completed string-manipulation-3 exercise

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -1,86 +1,98 @@
 /* eslint-disable no-unused-vars */
 
-// function titleCase(title) {
-//   var stringArray = title.split(' ');
-//   var lastIndex = stringArray.length - 1;
-//   var newArray = [];
+// capitalize the first word of the title and of any subtitle
 
-//   for (var i = 0; i < stringArray.length; i++) {
-//     if (stringArray[i].length <= 3 && stringArray[i] !== stringArray[0] && stringArray[i] !== stringArray[lastIndex]) {
-//       newArray.push(stringArray[i].toLowerCase());
-//     } else {
-//       newArray.push(stringArray[i].charAt(0).toUpperCase() + stringArray[i].substring(1));
-//     }
-//   }
-//   return newArray.join(' ');
-// }
+// capitalize all "major" words (nouns, verbs, adjectives, adverbs, and pronouns) in the title, including the second part of
+// hyphenated major words (e.g., Self-Report, not Self-report)
 
-// capitalize the first word if the title and of any subtitle
-// capitalize all "major" words in the title, including the second part of hyphenated major words
 // capitalize all words of four letters or more
 
-// function titleCase(title) {
-//   var stringArray = title.split(' ');
-//   var newArray = [];
+// this boils down to using lowercase only for "minor" words of three letters or fewer, namely, for conjunctions
+// (words like and, or, nor, and but), articles (a, an, and the), and prepositions (as, at, by, for, in, of, on, per, and to),
+// as long as they aren't the first word in a title or subtitle
 
-//   for (var i = 0; i < stringArray.length; i++) {
-//     if (stringArray[i].length >= 4 && stringArray[i] !== stringArray[i].includes('-') && stringArray[i].toLowerCase() !== 'javascript') {
-//       newArray.push(stringArray[i].charAt(0).toUpperCase() + stringArray[i].substring(1));
-//     } else if (stringArray[i].toLowerCase().includes('javascript')) {
-//       newArray.push('JavaScript');
-//     } else if (stringArray[i].toLowerCase() === 'api') {
-//       newArray.push('API');
-//     } else if (stringArray[i].toLowerCase() === 'web') {
-//       newArray.push('Web');
-//     } else if (stringArray[i].includes('-')) {
-//       var hyphenArray = stringArray[i].split('-');
-//       newArray.push(hyphenArray[0].charAt(0).toUpperCase() + hyphenArray[0].substring(1) + '-' + hyphenArray[1].charAt(0).toUpperCase() + hyphenArray[1].substring(1));
-//     } else {
-//       newArray.push(stringArray[i].toLowerCase());
-//     }
-//   }
-//   newArray = newArray.join(' ');
-
-//   return newArray;
-// }
+// special cases
+// JavaScript is always JavaScript
+// API is a "known" acronym
 
 function titleCase(title) {
-  var stringArray = title.split(' ');
-  var trashArray = [];
-  var newArray = [];
-  var lastIndex = stringArray.length - 1;
+  var titleArray = title.split(' ');
+  var resultArray = [];
 
-  for (var i = 0; i < stringArray.length; i++) {
-    if (stringArray[i].includes('-')) {
-      var hyphenArray = stringArray[i].split('-');
-      newArray.push(hyphenArray[0].charAt(0).toUpperCase() + hyphenArray[0].substring(1) + '-' + hyphenArray[1].charAt(0).toUpperCase() + hyphenArray[1].substring(1));
-    } else if (stringArray[i].includes(':')) {
-      newArray.push(stringArray[i] + ' ' + stringArray[i + 1].charAt(0).toUpperCase() + stringArray[i + 1].substring(1));
-    } else if (stringArray[i].length >= 4 && stringArray[i] && stringArray[i].toLowerCase() !== 'javascript') {
-      newArray.push(stringArray[i].charAt(0).toUpperCase() + stringArray[i].substring(1));
-    } else if (stringArray[i].toLowerCase().includes('javascript')) {
-      newArray.push('JavaScript');
-    } else if (stringArray[i].toLowerCase() === 'api') {
-      newArray.push('API');
-    } else if (stringArray[i].toLowerCase() === 'web') {
-      newArray.push('Web');
-    } else if (stringArray[i] !== stringArray[0] && stringArray[i] !== stringArray[lastIndex]) {
-      newArray.push(stringArray[i].toLowerCase());
+  for (var i = 0; i < titleArray.length; i++) {
+    if (isSpecialCase(titleArray[i])) {
+      resultArray.push(specialCases(titleArray[i]));
+    } else if (minorWords(titleArray[i])) {
+      resultArray.push(titleArray[i].toLowerCase());
+    } else {
+      resultArray.push(capitalizeWords(titleArray[i]));
     }
   }
-  newArray = newArray.join(' ');
 
-  if (newArray.includes('An an')) {
-    newArray = newArray.replace('An an', 'An');
-  } else if (newArray.includes('The the')) {
-    newArray = newArray.replace('The the', 'The');
+  for (var x = 0; x < resultArray.length; x++) {
+    if (includesColon(resultArray[x])) {
+      resultArray[x + 1] = capitalizeWords(resultArray[x + 1]);
+    }
   }
 
-  if (newArray.includes('javascript:')) {
-    newArray = newArray.replace('javascript:', 'JavaScript:');
-  } else if (newArray.includes('Javascript:')) {
-    newArray = newArray.replace('Javascript:', 'JavaScript:');
-  }
+  return resultArray.join(' ');
+}
 
-  return newArray;
+// capitalize all "major" words (nouns, verbs, adjectives, adverbs, and pronouns) in the title, including the second part of
+// hyphenated major words (e.g., Self-Report, not Self-report)
+
+// capitalize all words of four letters or more
+
+function capitalizeWords(word) {
+  var result = '';
+  var firstLetter = word[0].toUpperCase();
+  var remainingLetters = word.substring(1).toLowerCase();
+
+  result = firstLetter + remainingLetters;
+
+  if (word.includes('-')) {
+    var hyphenIndex;
+    hyphenIndex = word.indexOf('-');
+
+    result = firstLetter + word.substring(1, hyphenIndex) + '-' + word[hyphenIndex + 1].toUpperCase() + word.substring(hyphenIndex + 2).toLowerCase();
+
+  }
+  return result;
+}
+
+function minorWords(word) {
+  if (word.length <= 3) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function isSpecialCase(word) {
+  if (word.toLowerCase() === 'javascript' || word.toLowerCase() === 'javascript:' || word.toLowerCase() === 'api' || word.toLowerCase() === 'web') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function includesColon(word) {
+  if (word.includes(':')) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function specialCases(word) {
+  if (word.toLowerCase() === 'api') {
+    word = word.toUpperCase();
+  } else if (word.toLowerCase() === 'web') {
+    word = 'Web';
+  } else if (word.toLowerCase() === 'javascript') {
+    word = 'JavaScript';
+  } else if (word.toLowerCase() === 'javascript:') {
+    word = 'JavaScript:';
+  }
+  return word;
 }
