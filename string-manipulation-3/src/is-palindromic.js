c8e116ca1ad705f6e64a7ff2e60f7537fed808d9
Dean Vo <deaanvo@gmail.com>
2020-11-22 18:54:24 -0800

    removed commented code

--- string-manipulation-3/src/is-palindromic.js
+++ string-manipulation-3/src/is-palindromic.js
@@ -1,21 +1,5 @@
 /* eslint-disable no-unused-vars */
 
-// function isPalindromic(string) {
-//   var reversedString = '';
-//   var result;
-
-//   for (var i = string.length - 1; i >= 0; i--) {
-//     reversedString += string[i];
-//   }
-
-//   if (string === reversedString) {
-//     result = true;
-//   } else {
-//     result = false;
-//   }
-//   return result;
-// }
-
 function isPalindromic(string) {
   var reversedString = '';
   var result;
