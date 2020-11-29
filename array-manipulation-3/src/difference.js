58193cb7d9c1623d0878f062ac700bbde14b465e
Dean Vo <deaanvo@gmail.com>
2020-11-28 22:38:58 -0800

    will continue intersection and union later

--- array-manipulation-3/src/difference.js
+++ array-manipulation-3/src/difference.js
@@ -1,1 +1,27 @@
 /* eslint-disable no-unused-vars */
+function difference(first, second) {
+  let joinedArray = first.concat(second);
+
+  joinedArray = unique(joinedArray);
+
+  for (let i = 0; i < first.length; i++) {
+    for (let y = 0; y < second.length; y++) {
+      for (let x = 0; x < joinedArray.length; x++) {
+        if (first[i] === joinedArray[x] && second[y] === joinedArray[x]) {
+          joinedArray.splice(x, 1);
+        }
+      }
+    }
+  }
+  return joinedArray;
+}
+
+function unique(array) {
+  const resultArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (resultArray.indexOf(array[i]) === -1) {
+      resultArray.push(array[i]);
+    }
+  }
+  return resultArray;
+}
--- array-manipulation-3/src/zip.js
+++ array-manipulation-3/src/zip.js
@@ -1,1 +1,13 @@
 /* eslint-disable no-unused-vars */
+
+function zip(first, second) {
+  const resultArray = [];
+
+  for (let i = 0; i < first.length && i < second.length; i++) {
+    const newArray = [];
+    newArray.push(first[i]);
+    newArray.push(second[i]);
+    resultArray.push(newArray);
+  }
+  return resultArray;
+}
