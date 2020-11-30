a2ecd45d52761df9c61566b0959a9aa4c88c5c4a
Dean Vo <deaanvo@gmail.com>
2020-11-29 17:12:06 -0800

    completed array manipulation 3 exercise

--- array-manipulation-3/src/intersection.js
+++ array-manipulation-3/src/intersection.js
@@ -1,1 +1,28 @@
 /* eslint-disable no-unused-vars */
+
+function intersection(first, second) {
+  const joinedArray = first.concat(second);
+  let resultArray = [];
+
+  for (let i = 0; i < first.length; i++) {
+    for (let x = 0; x < second.length; x++) {
+      for (let y = 0; y < joinedArray.length; y++) {
+        if (first[i] === joinedArray[y] && second[x] === joinedArray[y]) {
+          resultArray.push(first[i]);
+          resultArray = unique(resultArray);
+        }
+      }
+    }
+  }
+  return resultArray;
+}
+
+function unique(array) {
+  const resultArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (resultArray.indexOf(array[i]) === -1) {
+      resultArray.push(array[i]);
+    }
+  }
+  return resultArray;
+}
--- array-manipulation-3/src/union.js
+++ array-manipulation-3/src/union.js
@@ -1,1 +1,17 @@
 /* eslint-disable no-unused-vars */
+
+function union(first, second) {
+  let joinedArray = first.concat(second);
+  joinedArray = unique(joinedArray);
+  return joinedArray;
+}
+
+function unique(array) {
+  const resultArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (resultArray.indexOf(array[i]) === -1) {
+      resultArray.push(array[i]);
+    }
+  }
+  return resultArray;
+}
