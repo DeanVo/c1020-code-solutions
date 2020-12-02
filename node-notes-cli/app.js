b9f54e571b00a85181eb5f70e90d98b0c68016b9
Dean Vo <deaanvo@gmail.com>
2020-12-02 11:56:36 -0800

    removed json.nextId modifiers except for when new note is added. changed json output for read function

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -3,18 +3,13 @@
 const fs = require('fs');
 const json = require('./data.json');
 
-const keys = Object.keys(json.notes);
-const lastKey = keys[keys.length - 1];
-
 if (process.argv[2] === 'read') {
-  console.log(JSON.stringify(json.notes, null, 2));
+  for (const key in json.notes) {
+    console.log(`${key}: ${json.notes[key]}`);
+  }
 } else if (process.argv[2] === 'create') {
   json.notes[json.nextId] = process.argv[3];
-  if (keys.length === 0) {
-    json.nextId = 2;
-  } else {
-    json.nextId = Number(lastKey) + 2;
-  }
+  json.nextId++;
   fs.writeFile('data.json', JSON.stringify(json, null, 2), 'utf8', () => {
     console.log('Note added.');
   });
@@ -25,15 +20,7 @@ if (process.argv[2] === 'read') {
   });
 } else if (process.argv[2] === 'delete') {
   delete json.notes[process.argv[3]];
-  if (process.argv[3] === String(lastKey) && keys.length === 2) {
-    json.nextId = Number(keys[0]) + 1;
-  } else if (process.argv[3] === String(lastKey) && keys.length === 1) {
-    json.nextId = 1;
-  } else if (process.argv[3] === String(lastKey)) {
-    json.nextId = Number(lastKey);
-  } else {
-    json.nextId = Number(lastKey) + 1;
-  }
+
   fs.writeFile('data.json', JSON.stringify(json, null, 2), 'utf8', () => {
     console.log('Note deleted.');
   });
