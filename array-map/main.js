07cf1fc6e63ad6976be4f486a647dcd570bf71e0
Dean Vo <deaanvo@gmail.com>
2020-12-07 12:04:34 -0800

    completed array-map exercise

---
+++ array-map/main.js
@@ -0,0 +1,35 @@
+/* eslint-disable no-console, no-unused-vars */
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+const languages = [
+  'Hypertext Markup Language',
+  'Cascading Style Sheets',
+  'ECMAScript',
+  'PHP Hypertext Preprocessor',
+  'Structured Query Language'
+];
+
+const double = x => {
+  return x * 2;
+};
+
+const doubled = numbers.map(double);
+
+const priceFormat = number => {
+  return `$${number.toFixed(2)}`;
+};
+
+const prices = numbers.map(priceFormat);
+
+const toUpper = word => {
+  return word.toUpperCase();
+};
+
+const upperCased = languages.map(toUpper);
+
+const firstIndex = word => {
+  return word[0];
+};
+
+const firstLetters = languages.map(firstIndex);
