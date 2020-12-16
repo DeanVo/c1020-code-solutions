39312c7a4418bd7ff893095456479315100ea10b
Dean Vo <deaanvo@gmail.com>
2020-12-15 17:54:17 -0800

    completed react-validated-input-component exercise

--- react-validated-input-component/src/validated-input.jsx
+++ react-validated-input-component/src/validated-input.jsx
@@ -15,19 +15,34 @@ export default class ValidatedInput extends React.Component {
 
   render() {
     let icon;
-    let error;
     let message;
+    let messageDigit;
+    let messageCap;
+    let messageSpecial;
+    const regexrDigit = new RegExp('(?=.*?[0-9])');
+    const regexrCapital = new RegExp('(?=.*?[A-Z])');
+    const regexrSpecial = new RegExp('(?=.?[!@#$%^&*()])');
 
     if (this.state.input.length === 0) {
       icon = 'fas fa-times icon red';
-      error = 'error-message';
       message = 'A password is required.';
     } else if (this.state.input.length < 8) {
       icon = 'fas fa-times icon red';
-      error = 'error-message';
       message = 'Your password is too short.';
     } else {
       icon = 'fas fa-check icon green';
+    }
+
+    if (!regexrDigit.test(this.state.input)) {
+      messageDigit = 'You need at least one digit.';
+    }
+
+    if (!regexrCapital.test(this.state.input)) {
+      messageCap = 'You need at least one capital letter.';
+    }
+
+    if (!regexrSpecial.test(this.state.input)) {
+      messageSpecial = 'You need at least one special character. (!, @, #, $, %, ^, &, *, (, or ))';
     }
 
     return (
@@ -39,7 +54,12 @@ export default class ValidatedInput extends React.Component {
           </form>
           <i className={icon}></i>
         </div>
-        <div className={error}>{message}</div>
+        <div className='error-message'>{message}</div>
+        <p className='error-message'>
+          {messageDigit}<br></br>
+          {messageCap}<br></br>
+          {messageSpecial}
+        </p>
       </div>
     );
   }
